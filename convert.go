bb6214d823f2e4779536808344fca0f7b3d52e26
Andy Song <wsong.cn@gmail.com>
2012-03-09 01:57:23 +0800

    maybe need to fix resize()

--- convert.go
+++ convert.go
@@ -4,61 +4,44 @@ package main
 
 import (
     "fmt"
+    "darts"
     "os"
     "bufio"
-    "strings"
+    "bytes"
+    "sort"
     "strconv"
     "encoding/gob"
     )
 
-type Thesaurus struct{
-    Offset int;
-    Body string;
-}
-type DictItem struct{
-    Freq int;
-    ThesaurusCount int;
-    TT [8]Thesaurus;
-}
-
 func main(){
-    dict := make(map[string]*DictItem);
-    unifile, _ := os.Open("unigram.txt");
+    strings := make([]string, 0, 4096)
+    keys := make([][]byte, 0, 4096)
+    values := make([]int, 0, 4096)
+    unifile, _ := os.Open("darts.txt");
     defer unifile.Close();
-    thesaurusfile, _ := os.Open("thesaurus.txt");
-    defer thesaurusfile.Close();
-    ofile, _ := os.Create("uni.lib");
+    ofile, _ := os.Create("darts.lib");
     defer ofile.Close();
 
-    uniLineReader, _ := bufio.NewReaderSize(unifile, 400);
+    uniLineReader := bufio.NewReaderSize(unifile, 400);
     line, _, bufErr := uniLineReader.ReadLine();
     for nil == bufErr {
-	rst := strings.Split(string(line)[:], "\t");
-	key := rst[0];
-	freq, _ := strconv.Atoi(rst[1]);
+	strings = append(strings, string(line))
 	//fmt.Printf("This is a line: %v:%d, end\n", rst[0], freq);
-	dict[key] = &DictItem{freq, 0, [8]Thesaurus{}};
 	line, _, bufErr = uniLineReader.ReadLine();
     }
-
-    thesaurusLineReader, _ := bufio.NewReaderSize(thesaurusfile, 400);
-    line, _, bufErr = thesaurusLineReader.ReadLine();
-    for nil == bufErr {
-	key := string(line);
-	line, _, bufErr = thesaurusLineReader.ReadLine();
-	ttts := strings.Split(string(line)[:], "\t");
-//	fmt.Printf("key=%v\n", key);
-	dict[key].ThesaurusCount = len(ttts);
-	for i:=0; i<len(ttts); i++{
-	    dict[key].TT[i] = Thesaurus{strings.Index(key, ttts[i]), ttts[i]}
-//	    fmt.Printf("\t%v end\n", ttts[i]);
-	}
-	line, _, bufErr = thesaurusLineReader.ReadLine();
+    sort.Strings(strings)
+    for _, s := range strings{
+	rst := bytes.Split([]byte(s), []byte("\t"));
+	freq, _ := strconv.Atoi(string(rst[1]));
+	keys = append(keys, rst[0])
+	values = append(values, freq)
     }
+
 //    for key, value := range dict {
 //	fmt.Printf("key %s, value %d\n", key, value)
 //    }
-    fmt.Printf("dict length: %v\n", len(dict));
+    fmt.Printf("dict length: %v %v\n", len(keys), len(values));
+    d := darts.Build(keys, values)
     enc := gob.NewEncoder(ofile);
-    enc.Encode(dict);
+    enc.Encode(d);
 }
